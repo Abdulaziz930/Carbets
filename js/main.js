99b35270e5eb565e59700f397fe594ed0cdc1d25
Abdulaziz930 <abdulazizak@code.edu.az>
2021-04-04 01:00:15 +0400

    projects section is done

--- js/main.js
+++ js/main.js
@@ -11,3 +11,38 @@ hamburgerMenu.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+$(document).ready(function () {
+  $(".project-slider").slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
